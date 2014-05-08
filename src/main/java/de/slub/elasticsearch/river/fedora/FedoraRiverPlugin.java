e1a7f7ad67dcb8ab5e853ac822ce5c4e02fdb915
Ralf Claussnitzer <ralf.claussnitzer@gmail.com>
2014-05-08 23:47:37 +0200

    Adapting plugin code to documentation

--- src/main/java/de/slub/elasticsearch/river/fedora/FedoraRiverPlugin.java
+++ src/main/java/de/slub/elasticsearch/river/fedora/FedoraRiverPlugin.java
@@ -1,25 +1,28 @@
 package de.slub.elasticsearch.river.fedora;
 
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.plugins.AbstractPlugin;
 import org.elasticsearch.river.RiversModule;
 
 public class FedoraRiverPlugin extends AbstractPlugin {
     @Inject
-    public FedoraRiverPlugin() {
+    public FedoraRiverPlugin(Settings settings) {
+        super();
     }
 
     @Override
     public String name() {
-        return "river-fedora";
+        return "fedora-river";
     }
 
     @Override
     public String description() {
-        return "River Fedora Plugin";
+        return "Fedora river plugin";
     }
 
+    @SuppressWarnings("UnusedDeclaration")
     public void onModule(RiversModule module) {
-        module.registerRiver("fedora", FedoraRiverModule.class);
+        module.registerRiver("fedora-river", FedoraRiverModule.class);
     }
 }
